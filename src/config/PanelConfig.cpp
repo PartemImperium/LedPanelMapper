cfd2c14c1887632079cd968c6879a6a300b78e8d
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2021-10-11 00:22:11 -0500

    Move Panel Config ofParameter to overall set instead of several sets for each prop

--- src/config/PanelConfig.cpp
+++ src/config/PanelConfig.cpp
@@ -3,16 +3,10 @@
 void PanelConfig::setup(ofxJSONElement json, int panelNumber) {
     setName("Panel (" + std::to_string(panelNumber) + ")");
         
-    X = json["x"].asInt();
-    X.setName("Source X");
-    X.setMin(0);
-    X.setMax(1920);//TODO: Decide what to put for max/ how to get a logical max based on what the source res is
+    X.set("Source X", json["x"].asInt(), 0, 1920);//TODO: Decide what to put for max/ how to get a logical max based on what the source res is
     add(X);
     
-    Y = json["y"].asInt();
-    Y.setName("Source Y");
-    Y.setMin(0);
-    Y.setMax(1080);//TODO: Decide what to put for max/ how to get a logical max based on what the source res is
+    Y.set("Source Y", json["y"].asInt(), 0, 1080);//TODO: Decide what to put for max/ how to get a logical max based on what the source res is
     add(Y);
 }
 
--- src/config/PanelGroupConfig.cpp
+++ src/config/PanelGroupConfig.cpp
@@ -3,30 +3,17 @@
 void PanelGroupConfig::setup(ofxJSONElement json) {
     setName("Panel Info");
     
-    PanelWidth = json["panelWidth"].asInt();
-    PanelWidth.setName("Panel Width");
-    PanelWidth.setMin(0);
-    PanelWidth.setMax(512);// TODO: Figure out what to put for max value
-    
+    PanelWidth.set("Panel Width", json["panelWidth"].asInt(), 0, 512);// TODO: Figure out what to put for max value
     add(PanelWidth);
     
-    PanelHeight = json["panelHeight"].asInt();
-    PanelHeight.setName("Panel Height");
-    PanelHeight.setMin(0);
-    PanelHeight.setMax(512);// TODO: Figure out what to put for the max value
+    PanelHeight.set("Panel Height",json["panelHeight"].asInt(),0,512);// TODO: Figure out what to put for the max value
     add(PanelHeight);
     
     
-    PanelsToFillWidth = json["panelsToFillWidth"].asInt();
-    PanelsToFillWidth.setName("Number of Panels to Fill Width");
-    PanelsToFillWidth.setMin(1);
-    PanelsToFillWidth.setMax(32);// TODO: Figure out what to put for the max value
+    PanelsToFillWidth.set("Number of Panels to Fill Width", json["panelsToFillWidth"].asInt(), 1, 32);// TODO: Figure out what to put for the max value
     add(PanelsToFillWidth);
     
-    PanelsToFillHeight = json["panelsToFillHeight"].asInt();
-    PanelsToFillHeight.setName("Number of Panels to Fill Height");
-    PanelsToFillHeight.setMin(1);
-    PanelsToFillHeight.setMax(32);// TODO: Figure out what to put for the max value
+    PanelsToFillHeight.set("Number of Panels to Fill Height", json["panelsToFillHeight"].asInt(),1,32);// TODO: Figure out what to put for the max value
     add(PanelsToFillHeight);
     
 	ofxJSONElement panels = json["panels"];
